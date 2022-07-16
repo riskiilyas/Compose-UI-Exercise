09018e4150c8fe9b0827a2ccaa0704f0b118849a
riskiilyas <riskiilyas03@gmail.com>
2022-07-16 19:22:51 +0700

    Initial commit

---
+++ app/src/main/java/com/keecoding/compose_ui_exercise/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.keecoding.compose_ui_exercise.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
